b936cf4515405e7444472b11df68ae5641aac781
Peter Organisciak <organisciak@gmail.com>
2021-05-03 19:19:49 -0600

    Add question text

--- scripting_grading/lab2.py
+++ scripting_grading/lab2.py
@@ -1,4 +1,5 @@
 from scripting_grading.grading import Answer
+import itertools
 
 class L2_Q1_Answers(Answer):
     
@@ -92,43 +93,65 @@ class L2_Q13_Answers(Answer):
             pts = 0
             err = 'Try "HEADING".center(15, "=") (or set "HEADING" to a variable first)'
         return pts, err
+
+sql_perm = lambda x,y,z: [" AND ".join(x) for x in itertools.permutations([x,y,z])]
     
 key = dict(
-    q1 = dict(entrytype='cell', pts=5, auto=True, answers=L2_Q1_Answers),
-    q2 = dict(entrytype='var', pts=5, auto=False),
-    q3 = dict(entrytype='cell', pts=5, auto=True, answers=L2_Q3_Answers),
+    q1 = dict(entrytype='cell', pts=5, auto=True, answers=L2_Q1_Answers,
+             question="Write the SQL to add these two rows to the `worker_wages` table"),
+    q2 = dict(entrytype='var', pts=5, auto=False,
+             question="What's wrong with this SQL?"),
+    q3 = dict(entrytype='cell', pts=5, auto=True, answers=L2_Q3_Answers,
+             question="What's the SQL to create the table for this dataset? Include appropriate data types for the columns and call it 'heights'"),
     q4a = dict(entrytype='var', pts=5, auto=True,
                answers=["sex == 'M' AND repht > 100"],
-               filters=['sql_where_clean']
+               filters=['sql_where_clean'],
+             question="The men that say they're taller than 100cm?"
               ),
     q4b = dict(entrytype='var', pts=5, auto=True,
                answers=['height < repht', 'repht > height'],
-               filters=['sql_where_clean']
+               filters=['sql_where_clean'],
+             question="The people that say they're taller than they are?"
               ),
     q4c = dict(entrytype='var', pts=10, auto=True,
-               answers=['sex == "F" AND height < repht AND weight < repwt', 
-                        'sex == "F" AND repht > height AND weight < repwt', 
-                        'sex == "F" AND height < repht AND repwt > weight', 
-                        'sex == "F" AND repht > height AND repwt > weight'],
-               filters=['sql_where_clean']
+               answers=(sql_perm('sex == "F"', 'height < repht', 'weight < repwt') +
+                        sql_perm('sex == "F"', 'repht > height', 'weight < repwt') +
+                        sql_perm('sex == "F"', 'height < repht', 'repwt > weight') +
+                        sql_perm('sex == "F"', 'repht > height', 'repwt > weight')),
+               filters=['sql_where_clean'],
+             question="The women that overestimate their weight and underestimate their height?"
               ),
-    q5 = dict(entrytype='var', pts=5, auto=False),
-    q6 = dict(entrytype='cell', pts=5, auto=False),
-    q7 = dict(entrytype='cell', pts=5, auto=True, answers=L2_Q7_Answers),
+    q5 = dict(entrytype='var', pts=5, auto=False,
+             question="Describe in regular English what the query does"),
+    q6 = dict(entrytype='cell', pts=5, auto=False,
+             question="Write a loop that prints the numbers from 1 to 5."),
+    q7 = dict(entrytype='cell', pts=5, auto=True, answers=L2_Q7_Answers,
+             question="Set a variable x to 0, then write a loop to add the following numbers to it: 1,1,2,3,5,8,13,21."),
     # TODO future iteration - 8a question is confusing because of the space
     # For now allowing both answer
-    q8a = dict(entrytype='var', pts=4, auto=True, answers=['False', 'True']),
-    q8b = dict(entrytype='var', pts=4, auto=True, answers=['True']),
-    q8c = dict(entrytype='var', pts=4, auto=True, answers=['True']),
+    q8a = dict(entrytype='var', pts=4, auto=True, answers=['False', 'True'],
+             question='"hello"+"world" == "hello world"'),
+    q8b = dict(entrytype='var', pts=4, auto=True, answers=['True'],
+             question="2 != 3"),
+    q8c = dict(entrytype='var', pts=4, auto=True, answers=['True'],
+             question="'a' < 'b'"),
     # TODO clarify language of 'equivalence'
-    q9a = dict(entrytype='var', pts=4, auto=True, answers=['No']),
-    q9b = dict(entrytype='var', pts=4, auto=True, answers=['Yes']),
+    q9a = dict(entrytype='var', pts=4, auto=True, answers=['No'],
+             question="In Python, are `1` (an integer) and `'1'` (a string) equivalent?"),
+    q9b = dict(entrytype='var', pts=4, auto=True, answers=['Yes'],
+             question="What about `1` and `1.0`?"),
     # TODO future iteration, lower pts here, add them to q7 instead
-    q10 = dict(entrytype='var', pts=8, auto=False),
+    q10 = dict(entrytype='var', pts=8, auto=False,
+             question="Functionally, what is the difference between the following two code blocks?"),
     # TODO future iteration, lower pts here
-    q11 = dict(entrytype='var', pts=7, auto=True, answers=['False']),
+    q11 = dict(entrytype='var', pts=7, auto=True, answers=['False'],
+             question="True or False: A list cannot have another list inside it."),
     q12 = dict(entrytype='var', pts=5, auto=True, answers=[
+        "the string is an alphabetic string,",
+        "the string is an alphabetic string",
         "all characters in S are alphabetic and there is at least one character in S"
-    ]),
-    q13 = dict(entrytype='cell', pts=10, auto=True, answers=L2_Q13_Answers),
+    ],
+             question='What does `test.isalpha()` do? Fill in the blank: "Return True if {{   }}, False otherwise."'),
+    q13 = dict(entrytype='cell', pts=10, auto=True, answers=L2_Q13_Answers,
+             question='What\'s the code to change "HEADING" to "====HEADING===="?'),
 )
